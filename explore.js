50c972df18647a8ecc5cdb15161cefa71a12c6b6
nikhil1028 <nikhil.raj.cse13@itbhu.ac.in>
2017-01-03 09:30:21 +0530

    updated explorer.js and signup.css
    explore.js : remove double scrolls
    signup.css : full signup overlay on any explorer

--- explore.js
+++ explore.js
@@ -1,8 +1,10 @@
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
+	document.getElementsByTagName("body")[0].style.overflowY = "hidden";
 }
 
 /* Close when someone clicks on the "x" symbol inside the explore */
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+	document.getElementsByTagName("body")[0].style.overflowY = "scroll";
 }
